c53610dd89f311f22cb93ab358ad486ad94c9c3d
Hoang Nè <levuthanhtung11@gmail.com>
2025-06-05 14:44:27 +0700

    feat: update version 0.3.0
    feat: update version 0.3.0

--- sshr.rb
+++ sshr.rb
@@ -1,10 +1,10 @@
 class Sshr < Formula
   desc "A TUI for managing and connecting to SSH hosts"
   homepage "https://github.com/hoangneeee/sshr"
-  url "https://github.com/hoangneeee/sshr/releases/download/v0.2.0/sshr-x86_64-apple-darwin.tar.gz"
-  sha256 "8e42c85dac9625c13649f5496705fcb93fce071d23faf018f60d78cb794ea03d" # "shasum -a 256 <SHA256 của file tar.gz>"
+  url "https://github.com/hoangneeee/sshr/releases/download/v0.3.0/sshr-x86_64-apple-darwin.tar.gz"
+  sha256 "99bff2c0e1c573c6f31a861c80092fbb247384b4d48ed1f916663efcae3527b5" # "shasum -a 256 <SHA256 của file tar.gz>"
   license "Apache-2.0"
-  version "0.2.0"
+  version "0.3.0"
 
   def install
     bin.install "sshr"
