395deff03b730c4afe4dbbb791edc1ff1c4d2603
Hoang Nè <levuthanhtung11@gmail.com>
2025-06-08 18:06:11 +0700

    chore: update version

--- sshr.rb
+++ sshr.rb
@@ -1,10 +1,10 @@
 class Sshr < Formula
   desc "A TUI for managing and connecting to SSH hosts"
   homepage "https://github.com/hoangneeee/sshr"
-  url "https://github.com/hoangneeee/sshr/releases/download/v0.4.0/sshr-x86_64-apple-darwin.tar.gz"
-  sha256 "34c3c4a002f49674771583570c237fb87d79c14e6f06d23d113a1f0e5050f24f"
+  url "https://github.com/hoangneeee/sshr/releases/download/v0.5.0/sshr-x86_64-apple-darwin.tar.gz"
+  sha256 "9e0e653770aac6c30da758ff2cc08be563876f53393dc8e225101be31f32ed05"
   license "Apache-2.0"
-  version "0.4.0"
+  version "0.5.0"
 
   def install
     bin.install "sshr"
